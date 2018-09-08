671a0c01768d3613f548e5e96dfd8a6b085032e6
Ziyu0 <yuliazhouziyu@gmail.com>
2018-09-08 12:40:48 -0500

    Add initialial files for hw2

---
+++ homework/hw2/main.py
@@ -0,0 +1,23 @@
+from io_tools import load_data
+
+def main():
+    """High level pipeline."""
+
+    # Load dataset.
+    data = load_data("data/MNISTdata.hdf5")
+    train_data, test_data = data[:2], data[2:]
+    print(train_data[0].shape, test_data[0].shape)
+
+    # Init model.
+    # nn = NeuralNetwork(train_data[0].shape[1], hidden_units=100)
+
+    # # Train.
+    # print("training model...")
+    # nn.train(train_data, learning_rate=0.1, epochs=25)
+
+    # # Test
+    # print("testing model...")
+    # nn.test(test_data)
+
+if __name__ == "__main__":
+    main()
