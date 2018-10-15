575bcb4749c8a03c1e4a36da24c8bab72416bbfb
Ziyu0 <yuliazhouziyu@gmail.com>
2018-10-14 21:51:29 -0500

    Finish data sampling

--- homework/hw5/dataset.py
+++ homework/hw5/dataset.py
@@ -10,9 +10,18 @@ import torchvision.transforms as transforms
 import random
 
 class TinyImageNetDataset(Dataset):
-    def __init__(self, root, train=True, transform=None):
+    def __init__(self, file_paths, train=True, transform=None):
         self.train = train
-    
+        self.transform = transform
+
+        self.image_paths = []
+        self.labels = []
+        with open(file_paths, "r") as file:
+            for line in file:
+                path, label = line.strip().split()
+                self.image_paths.append(path)
+                self.labels.append(label)
+   
     def __getitem__(self, index):
         """
         To make it easier to understand, just do online triplet sampling.
@@ -21,27 +30,54 @@ class TinyImageNetDataset(Dataset):
         => so don't need the triplet_list.txt file
         """
 
-        step = 500
-        count = index // step
-        start = count * step
-        end = start + step - 1
-
-        # Query image
-        query_idx = index
-
-        # Sample positive image within the same class
-        positive_idx = random.randint(start, end)
-        while positive_idx == query_idx:
-            positive_idx = random.randint(start, end)
+        if not self.train:
+            img = Image.open(self.image_paths[index])
+            img = img.convert('RGB')
+            label = self.labels[index]
+            if self.transform is not None:
+                img = self.transform(img)
+            return img, label
         
-        # Sample positive image in other class
-        range1 = list(range(0, start))
-        range2 = list(range(end + 1, total))
-        negative_idx = random.sample(range1 + range2, 1)[0]
+        else:
+            step = 500
+            count = index // step
+            start = count * step
+            end = start + step - 1
+            total = 100000
 
-        # Load the three images
+            # Query image
+            query_idx = index
 
-        pass
+            # Sample positive image within the same class
+            positive_idx = random.randint(start, end)
+            while positive_idx == query_idx:
+                positive_idx = random.randint(start, end)
+            
+            # Sample positive image in other class
+            range1 = list(range(0, start))
+            range2 = list(range(end + 1, total))
+            negative_idx = random.sample(range1 + range2, 1)[0]
+
+            # Load the three images
+            q_img = Image.open(self.image_paths[query_idx])
+            p_img = Image.open(self.image_paths[positive_idx])
+            n_img = Image.open(self.image_paths[negative_idx])
+            q_img = q_img.convert('RGB')
+            p_img = p_img.convert('RGB')
+            n_img = n_img.convert('RGB')
+
+            q_label = self.labels[query_idx]
+            p_label = self.labels[positive_idx]
+            n_label = self.labels[negative_idx]
+
+            # Transform
+            if self.transform is not None:
+                q_img = self.transform(q_img)
+                p_img = self.transform(p_img)
+                n_img = self.transform(n_img)
+
+            # Return tuple of the three images and tuple of the labels
+            return (q_img, p_img, n_img), (q_label, p_label, n_label)
     
     def __len__(self):
-        pass
+        return len(self.image_paths)
--- homework/hw5/main.py
+++ homework/hw5/main.py
@@ -1,17 +1,23 @@
+import torch
+import torchvision.transforms as transforms
+
 from utils import generate_train_img_names, generate_val_img_names
+from dataset import TinyImageNetDataset
 
 import os.path
+import multiprocessing
+import matplotlib.pyplot as plt
 
 import argparse
-# def str2bool(v):
-#     return v.lower() in ("yes", "true", "t", "1")
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
 
 parser = argparse.ArgumentParser(description="Image Ranking")
 parser.add_argument("--lr", default=0.001, type=float, help="learning rate")
 parser.add_argument("--epochs", default=40, type=int, help="number of training epochs")
 # parser.add_argument("--lr_schedule", default=True, type=str2bool, help="perform lr shceduling")
 # parser.add_argument("--load_checkpoint", default=False, type=str2bool, help="resume from checkpoint")
-# parser.add_argument("--show_sample_image", default=False, type=str2bool, help="display data insights")
+parser.add_argument("--show_sample_image", default=False, type=str2bool, help="display data insights")
 # parser.add_argument("--debug", default=False, type=str2bool, help="using debug mode")
 # parser.add_argument("--data_path", default="./data", type=str, help="path to store data")
 args = parser.parse_args()
@@ -28,6 +34,73 @@ def main():
     if (not os.path.isfile(train_list)) or (not os.path.isfile(val_list)):
         generate_train_img_names("data/tiny-imagenet-200/train/", train_list)
         generate_val_img_names("data/tiny-imagenet-200/val/", val_list)
+    
+    # Load data
+    print("==> Loading data...")
+    train_set = TinyImageNetDataset(
+        train_list, 
+        train=True,
+        transform=transforms.Compose([
+            transforms.Resize(224),
+            transforms.RandomCrop(224, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
+    )
+    val_set = TinyImageNetDataset(
+        val_list,
+        train=False,
+        transform=transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+    )
+
+    # shuffle or not ??
+    # num_workers = 32 on BW
+    workers = multiprocessing.cpu_count()
+    print("\tnumber of workers: {}".format(workers))
+    train_loader = torch.utils.data.DataLoader(
+        train_set,
+        batch_size=128,
+        shuffle=True,
+        num_workers=workers
+    )
+    val_loader = torch.utils.data.DataLoader(
+        val_set,
+        batch_size=128,
+        shuffle=False,
+        num_workers=workers
+    )
+
+    # Show sample image
+    if args.show_sample_image:
+        print("==> Loading image sample from a batch...")
+        data_iter = iter(train_loader)
+        images, labels = data_iter.next()  # Retrieve a batch of data
+        
+        print("list (len = {}) of batch images".format(len(images)))
+        print("a batch of query images in shape {}".format(images[0].shape))
+        print("a batch of positive images in shape {}".format(images[1].shape))
+        print("a batch of negative images in shape {}".format(images[2].shape))
+        
+        # Get a sampled triplet.
+        fig = plt.figure()
+        titles = ["query", "positive", "negative"]
+        for i in range(len(images)):
+            sample = images[i][0][0].numpy()
+            ax = plt.subplot(1, 3, i + 1)
+            plt.tight_layout()
+            ax.set_title(titles[i])
+            ax.axis('off')
+            plt.imshow(sample)
+            plt.savefig("sampled_triplet.png")
+
+            if i == 2:
+                plt.show()
+                break
 
 if __name__ == "__main__":
     main()
